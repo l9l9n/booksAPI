dd948d9054cb79e0c8dc93f766070eb072e7360c
l9L9n_PC_Ubuntu <l9l9ndos@gmail.com>
2023-10-31 03:44:56 +0600

    SetUP "Filters", "Search", "Ordering"

--- store/admin.py
+++ store/admin.py
@@ -5,6 +5,7 @@ from .models import Book
 @admin.register(Book)
 class AdminDisplay(admin.ModelAdmin):
     list_display = [
+        'id',
         'name',
         'price',
     ]
--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -7,13 +7,26 @@ from store.serializers import BooksSerializer
 
 
 class BooksApiTestCase(APITestCase):
+
+    def setUp(self):
+        self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1')
+        self.book_2 = Book.objects.create(name='Test book 2', price=35, author_name='Author 5')
+        self.book_3 = Book.objects.create(name='Test book Author 1', price=35, author_name='Author 3')
+
     def test_get(self):
-        book_1 = Book.objects.create(name='Test book 1', price=25)
-        book_2 = Book.objects.create(name='Test book 2', price=35)
+
         url = reverse('book-list')
 
         response = self.client.get(url)
-        serializer_data = BooksSerializer([book_1, book_2], many=True).data
+        serializer_data = BooksSerializer([self.book_1, self.book_2, self.book_3], many=True).data
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         self.assertEquals(serializer_data, response.data)
 
+    def test_get_filter(self):
+
+        url = reverse('book-list')
+
+        response = self.client.get(url, data={'search': "Author 1"})
+        serializer_data = BooksSerializer([self.book_1, self.book_3], many=True).data
+        self.assertEquals(status.HTTP_200_OK, response.status_code)
+        self.assertEquals(serializer_data, response.data)
