f78d0abf69a990c334edc4a7ba9a7721575f7553
l9l9n <l9l9ndos@gmail.com>
2023-10-30 18:17:17 +0600

    Change admin file

--- store/admin.py
+++ store/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Book
 
 
-# @admin.register
+@admin.register(Book)
+class AdminDisplay(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+    ]
+
