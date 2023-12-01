695e73544897dc1c9c1fe70c26265c5a2df83a41
l9l9n_HP <l9l9ndos@gmail.com>
2023-12-02 02:02:59 +0600

    TTD, Serializers method fields

--- store/serializers.py
+++ store/serializers.py
@@ -1,11 +1,17 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from .models import Book, UserBookRelation
 
 
 class BooksSerializer(ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author_name', 'likes_count')
+
+    def get_likes_count(self, instance):
+        return UserBookRelation.objects.filter(book=instance, like=True).count()
 
 
 class UserBookRelationSerializer(ModelSerializer):
--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -1,28 +1,45 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 from store.serializers import BooksSerializer
 
 
 class BookSerializerTest(TestCase):
     def test_ok(self):
+        user1 = User.objects.create(username='user1')
+        user2 = User.objects.create(username='user2')
+        user3 = User.objects.create(username='user3')
+
         book_1 = Book.objects.create(name='Test book 1', price=25,
                                      author_name='Author 1')
         book_2 = Book.objects.create(name='Test book 2', price=35,
                                      author_name='Author 2')
+
+        UserBookRelation.objects.create(user=user1, book=book_1, like=True)
+        UserBookRelation.objects.create(user=user2, book=book_1, like=True)
+        UserBookRelation.objects.create(user=user3, book=book_1, like=True)
+
+        UserBookRelation.objects.create(user=user1, book=book_2, like=True)
+        UserBookRelation.objects.create(user=user2, book=book_2, like=True)
+        UserBookRelation.objects.create(user=user3, book=book_2, like=False)
+
         data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test book 1',
                 'price': '25.00',
-                "author_name": 'Author 1',
+                'author_name': 'Author 1',
+                'likes_count': 3
+
             },
             {
                 'id': book_2.id,
                 'name': 'Test book 2',
                 'price': '35.00',
-                "author_name": 'Author 2',
+                'author_name': 'Author 2',
+                'likes_count': 2
             }
         ]
         self.assertEquals(expected_data, data)
